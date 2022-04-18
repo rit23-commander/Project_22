ccd6c90c7d74f45ec6444f1e68d850e77ec538fa
rit23-commander <singhkumar0253@gmail.com>
2022-04-18 10:11:15 -0400

    first virsion

---
+++ Project_connect22/app/src/main/java/com/example/project_connect22/log_fag.java
@@ -0,0 +1,22 @@
+package com.example.project_connect22;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class log_fag extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.log_fag , container , false);
+
+        return v ;
+    }
+}
